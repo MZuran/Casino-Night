fe571566929876d3f3401dfe50f1f9c851aea21e
Matías Zuran <mzuran@frba.utn.edu.ar>
2023-07-01 13:28:03 -0300

    PRIMERA PREENTREGA PROYECTO FINAL JAVASCRIPT

--- scripts/roullette.js
+++ scripts/roullette.js
@@ -1,6 +1,6 @@
 //Cómo generar un número al azar y cómo saber si un número era par o no lo busqué en google
 
-var playerMoney = 1000;
+var playerMoney = 1000
 
 function numberOfColor(roulletteNumber) {
   if (roulletteNumber == 0) {
@@ -30,23 +30,33 @@ function spinRoullette() {
   return Math.floor(Math.random() * 37)
 }
 
-function singleNumberBet(amount, number) {
+function nonRepeatingSingleNumberBet(amount, number) {
   let result = spinRoullette()
 
-  console.log("Se apostaron " + amount + "$ de los " + playerMoney + "$")
-  console.log("La ruleta gira y cae en " + result)
-  console.log(" ")
+  console.log('Se apostaron ' + amount + '$ de los ' + playerMoney + '$')
+  console.log('La ruleta gira y cae en ' + result)
+  console.log(' ')
 
   if (number == result) {
-    amount = amount * 35;
-    console.log("Se han ganado " + amount + "$")
+    amount = amount * 35
+    console.log('Se han ganado ' + amount + '$')
     playerMoney = playerMoney + amount
-    console.log("Pozo del jugador: " + playerMoney + "$")
-    console.log(" ")
+    console.log('Pozo del jugador: ' + playerMoney + '$')
+    console.log(' ')
   } else {
-    console.log("Se han perdido " + amount + "$")
+    console.log('Se han perdido ' + amount + '$')
     playerMoney = playerMoney - amount
-    console.log("Pozo del jugador: " + playerMoney + "$")
-    console.log(" ")
+    console.log('Pozo del jugador: ' + playerMoney + '$')
+    console.log(' ')
+  }
+}
+
+function singleNumberBet(amount, number, times) {
+  if (times) {
+    for (i = 0; i < times; i++) {
+      nonRepeatingSingleNumberBet(amount, number)
+    }
+  } else {
+    nonRepeatingSingleNumberBet(amount, number)
   }
 }
